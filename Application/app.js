5e20e9aa4afff64fee99188ffbd3cfe282f54305
Dennis-Stefan Toader <146251437+dennisnpc@users.noreply.github.com>
2023-11-24 17:40:35 +0000

    Added download encrypted file, random function, more
    Separated encrypt function from event listener for readability and reusability
    Added function to generate random number to use for encryption key
    Changed window event listener for DOM event listener for increased speed
    Added download function to download encrypted file to computer after it is encrypted
    Added check for file type when submitting file to encrypt (plain text only)
    Added '.enc' suffix to encrypted files

--- Application/app.js
+++ Application/app.js
@@ -1,41 +1,94 @@
-// Start once everything has loaded
-window.addEventListener("load", function (e) {
-    // Start once 'Encrypt' button is pressed
-    const fileSubmit = document.getElementById('submitEncFile');
-    fileSubmit.addEventListener('click', function (e) {
-        // Get input file
-        const fileInput = document.getElementById('fileInput');
-        // Check if a file was selected
-        if (fileInput.files[0]) {
-            const file = fileInput.files[0];
-            const reader = new FileReader();
-            let modifiedContents = '';
-            let key = '1234567';
+// Start once HTML elements have loaded
+document.addEventListener('DOMContentLoaded', function () {
 
-            // Read the file
-            reader.onload = function (event) {
-                const contents = event.target.result;
-
-                // Encryption algorithm
-                for (let i = 0; i < contents.length; i++) {
-                    let shift = Number(key[(contents.length + i) % key.length]);
-                    let currentCharCode = contents.charCodeAt(i);
-                    currentCharCode = currentCharCode + shift;
-                    modifiedContents += String.fromCharCode(currentCharCode);
-                }
-
-                // Output results
-                console.log('Original:', contents);
-                console.log('Encrypted:', modifiedContents);
-                console.log('Key:', key);
-            };
-
-            // Read file as text
-            reader.readAsText(file);
-        }
-        // If no file selected
-        else {
-            console.log('No file selected');
-        }
-    });
+    // 'Download' button reference
+    const encryptButton = document.getElementById('encFile');
+    // When the button is pressed
+    encryptButton.addEventListener('click', encrypt);
 });
+
+function encrypt() {
+
+    // Get input file
+    const fileInput = document.getElementById('fileInput');
+    // Check if a file was selected
+    if (fileInput.files[0] && fileInput.files[0].type === 'text/plain') {
+
+        const file = fileInput.files[0];
+        const reader = new FileReader();
+        let modifiedContents = '';
+        let key = randomNumber();
+
+        // Read the file
+        reader.onload = function (event) {
+            const contents = event.target.result;
+
+            // Encryption algorithm
+            for (let i = 0; i < contents.length; i++) {
+                let shift = Number(key[(contents.length + i) % key.length]);
+                let currentCharCode = contents.charCodeAt(i);
+                currentCharCode = currentCharCode + shift;
+                modifiedContents += String.fromCharCode(currentCharCode);
+            }
+
+            // Output results to console
+            console.log('Original:', contents);
+            console.log('Encrypted:', modifiedContents);
+            console.log('Key:', key);
+
+            // Download encrypted file
+            downloadFile(modifiedContents, file.name, '.enc');
+        };
+
+        // Read file as text
+        reader.readAsText(file);
+    }
+    // If no file selected
+    else if (!(fileInput.files[0])) {
+        console.log('No file selected');
+    }
+    // If incorrect file type selected
+    else if (!(fileInput.files[0].type === 'text/plain')) {
+        console.log('Incorrect file type');
+    }
+}
+
+function downloadFile(encryptedFile, originalName, extension) {
+
+    // Create a Blob containing the file content
+    const blob = new Blob([encryptedFile], { type: 'text/plain' });
+
+    // Create a link element
+    const aTag = document.createElement('a');
+    aTag.href = URL.createObjectURL(blob);
+
+    // Set the file name
+    let downloadName = originalName.toString();
+    downloadName += extension;
+    aTag.download = downloadName;
+
+    // Append the anchor element to the body
+    document.body.appendChild(aTag);
+
+    // Programmatically trigger a click event on the anchor element
+    aTag.click();
+
+    // Remove the anchor element from the body
+    document.body.removeChild(aTag);
+}
+
+function randomNumber() {
+
+    let number = '';
+
+    for (let i = 0; i < (Math.floor((Math.random() * 96) + 32)); i++) {
+        let newDigit;
+        do {
+            newDigit = (Math.floor((Math.random() * 9) + 1)).toString();
+        } while (i > 0 && newDigit === number[i - 1]);
+
+        number += newDigit;
+    }
+
+    return number;
+}
