79d1c8eb743a82d95dc388e2b867d13bfc98d67a
Dennis-Stefan Toader <146251437+dennisnpc@users.noreply.github.com>
2023-12-15 23:53:39 +0000

    decrypt function, download key button, upload encrypted file and key buttons

--- Application/app.js
+++ Application/app.js
@@ -1,12 +1,24 @@
 // Start once HTML elements have loaded
 document.addEventListener('DOMContentLoaded', function () {
 
-    // 'Download' button reference
-    const encryptButton = document.getElementById('encFile');
+    // 'Encrypt' button reference
+    const encryptButton = document.getElementById('getEncFile');
+
+    // 'Key' button reference
+    const getKeyButton = document.getElementById('getEncFileKey');
+
+    // 'Decrypt' button reference
+    const decryptButton = document.getElementById('getDecFile');
+
     // When the button is pressed
     encryptButton.addEventListener('click', encrypt);
+
+    decryptButton.addEventListener('click', decrypt);
+
+    getKeyButton.addEventListener('click', downloadKey);
 });
 
+let keyEnc;
 function encrypt() {
 
     // Get input file
@@ -17,7 +29,7 @@ function encrypt() {
         const file = fileInput.files[0];
         const reader = new FileReader();
         let modifiedContents = '';
-        let key = randomNumber();
+        keyEnc = randomNumber();
 
         // Read the file
         reader.onload = function (event) {
@@ -25,7 +37,7 @@ function encrypt() {
 
             // Encryption algorithm
             for (let i = 0; i < contents.length; i++) {
-                let shift = Number(key[(contents.length + i) % key.length]);
+                let shift = Number(keyEnc[(contents.length + i) % keyEnc.length]);
                 let currentCharCode = contents.charCodeAt(i);
                 currentCharCode = currentCharCode + shift;
                 modifiedContents += String.fromCharCode(currentCharCode);
@@ -34,15 +46,16 @@ function encrypt() {
             // Output results to console
             console.log('Original:', contents);
             console.log('Encrypted:', modifiedContents);
-            console.log('Key:', key);
+            console.log('Key:', keyEnc);
 
             // Download encrypted file
-            downloadFile(modifiedContents, file.name, '.enc');
+            downloadFile(modifiedContents, file.name, '.enc', 'text/plain');
         };
 
         // Read file as text
         reader.readAsText(file);
     }
+
     // If no file selected
     else if (!(fileInput.files[0])) {
         console.log('No file selected');
@@ -53,10 +66,96 @@ function encrypt() {
     }
 }
 
-function downloadFile(encryptedFile, originalName, extension) {
+function downloadKey() {
+
+    if (keyEnc != null) {
+        let encFileMetadata = {
+            key: keyEnc
+        };
+
+        let jsonFile = JSON.stringify(encFileMetadata);
+        downloadFile(jsonFile, 'key', '.json', 'application/json')
+    }
+
+    // If no file has been encrypted
+    else {
+        console.log('No has been generated');
+    }
+}
+
+function decrypt() {
+
+    // Get encrypted file input
+    const fileInput = document.getElementById('encFileInput');
+
+    // Check if a file was selected
+    if (fileInput.files[0]) {
+
+        const encFile = fileInput.files[0];
+        const keyInput = document.getElementById('encFileKeyInput');
+
+        // Get key input
+        if (keyInput.files[0] && keyInput.files[0].type === 'application/json') {
+            const keyFile = keyInput.files[0];
+            const reader = new FileReader();
+
+            reader.onload = function (event) {
+                const jsonContent = event.target.result;
+                const parsedJson = JSON.parse(jsonContent);
+
+                if (parsedJson && parsedJson.key) {
+                    console.log('Key from JSON:', parsedJson.key);
+
+                    const reader = new FileReader();
+                    let modifiedContents = '';
+                    let keyDec = parsedJson.key;
+
+                    // Read the key
+                    reader.onload = function (event) {
+                        const contents = event.target.result;
+
+                        // Decryption algorithm
+                        for (let i = 0; i < contents.length; i++) {
+                            let shift = Number(keyDec[(contents.length + i) % keyDec.length]);
+                            let currentCharCode = contents.charCodeAt(i);
+                            currentCharCode = currentCharCode - shift;
+                            modifiedContents += String.fromCharCode(currentCharCode);
+                        }
+
+                        // Output decrypted file
+                        downloadFile(modifiedContents, encFile.name, '');
+                    };
+
+                    reader.readAsText(encFile);
+                }
+                else {
+                    console.log('Invalid JSON format or key not found');
+                }
+            };
+
+            reader.readAsText(keyFile);
+        }
+
+        // If no file selected
+        else if (!(keyInput.files[0])) {
+            console.log('No key selected');
+        }
+        // If incorrect file type selected
+        else if (!(keyInput.files[0].type === 'application/json')) {
+            console.log('Incorrect key file type');
+        }
+    }
+
+    // If no file selected
+    else {
+        console.log('No file selected');
+    }
+}
+
+function downloadFile(file, originalName, extension, type) {
 
     // Create a Blob containing the file content
-    const blob = new Blob([encryptedFile], { type: 'text/plain' });
+    const blob = new Blob([file], { type: type });
 
     // Create a link element
     const aTag = document.createElement('a');
@@ -64,7 +163,12 @@ function downloadFile(encryptedFile, originalName, extension) {
 
     // Set the file name
     let downloadName = originalName.toString();
-    downloadName += extension;
+    if (downloadName.includes('.enc')) {
+        downloadName = downloadName.replace('.enc', '');
+    }
+    else {
+        downloadName += extension;
+    }
     aTag.download = downloadName;
 
     // Append the anchor element to the body
